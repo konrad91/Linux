00dd1a7bb809cb05ef9583b2498689aafa05b575
Konrad Johan Jensen <konradhtc@gmail.com>
2020-06-15 14:49:22 +0200

    Add files via upload

--- Rofi/powermenu.sh
+++ Rofi/powermenu.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
  
-res=$(echo " Logout|Reboot|Shutdown" | rofi -sep "|" -dmenu -i -p 'Power Menu: ' "" -theme powermenu.rasi)
+res=$(echo "  Logout|  Reboot|  Shutdown" | rofi -sep "|" -dmenu -i -p 'Power Menu: ' "" -theme powermenu.rasi)
  
-#if [[ $res = "Lock" ]]; then
-#    /home/khoaduccao/.config/lock.sh
-#fi
-if [[ $res = " Logout" ]]; then
+if [[ $res = "  Logout" ]]; then
     xfce4-session-logout --logout
 fi
-if [[ $res = "Reboot" ]]; then
+if [[ $res = "  Reboot" ]]; then
     xfce4-session-logout --reboot
 fi
-if [[ $res = "Shutdown" ]]; then
+if [[ $res = "  Shutdown" ]]; then
     xfce4-session-logout --halt
 fi
 exit 0
